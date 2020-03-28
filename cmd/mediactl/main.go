05d79af10218d23ba2a8cc337d4ff98ff00e1c8c
po3rin <abctail30@gmail.com>
2020-03-29 03:19:48 +0900

    add mediactl

--- cmd/mediactl/main.go
+++ cmd/mediactl/main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/po3rin/post/cmd/mediactl/store"
+)
+
+func isSupportedMedia(path string) bool {
+	e := filepath.Ext(path)
+	return e == ".png" || e == ".jpeg" || e == ".jpg" || e == ".gif"
+}
+
+type Uploader interface {
+	Upload(path string) (string, error)
+}
+
+type Walker struct {
+	Uploader
+	mdpath string
+}
+
+func NewWalker(uploader Uploader, dir string) *Walker {
+	return &Walker{
+		Uploader: uploader,
+		mdpath:   filepath.Join(dir, "post.md"),
+	}
+}
+
+func (w *Walker) Walk(path string, info os.FileInfo, err error) error {
+	if !isSupportedMedia(path) {
+		return nil
+	}
+
+	res, err := w.Upload(path)
+	if err != nil {
+		return errors.Wrap(err, "upload media")
+	}
+
+	b, err := ioutil.ReadFile(w.mdpath)
+	if err != nil {
+		return errors.Wrap(err, "read old file")
+	}
+
+	new := bytes.ReplaceAll(b, []byte(path), []byte(res))
+
+	f, err := os.Create(w.mdpath)
+	if err != nil {
+		return errors.Wrap(err, "create new file")
+	}
+
+	_, err = f.Write(new)
+	if err != nil {
+		return errors.Wrap(err, "write new body")
+	}
+
+	fmt.Println("----------------------")
+	fmt.Printf("%v\n↓\n%v\n", path, res)
+
+	// err = os.Remove()
+	// if err != nil {
+	// 	return "", errors.Wrap(err, "delete file")
+	// }
+
+	return nil
+}
 
 func main() {
-	fmt.Println("TODO: impliments")
+	id := flag.String("id", "", "post identifier (ex 2019/<< unixtime >>")
+	dir := flag.String("dir", "posts", "posts directory path")
+	bucketName := flag.String("bucket", "", "S3 bucket name")
+	flag.Parse()
+
+	postdir := filepath.Join(*dir, *id)
+	imgpath := filepath.Join(postdir, "img")
+
+	s := store.New(*bucketName, *id)
+	w := NewWalker(s, postdir)
+
+	err := filepath.Walk(imgpath, w.Walk)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/po3rin/post
 
 go 1.13
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/aws/aws-sdk-go v1.29.34
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)

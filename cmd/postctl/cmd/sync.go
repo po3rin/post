ebc9b104030b348013c3563e199785dda8c57d31
po3rin <abctail30@gmail.com>
2020-08-07 09:04:48 +0900

    api mode

--- cmd/postctl/cmd/sync.go
+++ cmd/postctl/cmd/sync.go
@@ -31,8 +31,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 )
@@ -42,6 +43,7 @@ const maxRetry = 10
 var (
 	url, root string
 	agentMode bool
+	port      int
 
 	oldRevision string
 )
@@ -49,16 +51,9 @@ var (
 func init() {
 	rootCmd.AddCommand(syncCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	syncCmd.Flags().StringVarP(&url, "url", "u", "", "API URL which handles posts")
 	syncCmd.Flags().StringVarP(&root, "root", "r", "", "Git repository root")
+	syncCmd.Flags().IntVarP(&port, "port", "p", 9300, "API port")
 	syncCmd.Flags().BoolVarP(&agentMode, "agent-mode", "a", false, "whether run with agent mode")
 }
 
@@ -73,7 +68,102 @@ type request struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-func allFilePath(workdir string) ([]string, error) {
+// newCmd represents the new command
+var syncCmd = &cobra.Command{
+	Use:   "sync",
+	Short: "sync post external store",
+	Long:  "sync post external store",
+	Run: func(cmd *cobra.Command, args []string) {
+		var filepaths []string
+		var err error
+		if !agentMode {
+			filepaths, err = postFiles(workdir)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			err = syncPost(filepaths)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			return
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		eg, ctx := errgroup.WithContext(ctx)
+
+		// git helthz with retry
+		eg.Go(func() error {
+			var retryCounter int
+			ticker := time.NewTicker(30 * time.Second)
+			for {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-ticker.C:
+					cmd := exec.Command("git", "status")
+					cmd.Dir = root
+					out, err := cmd.CombinedOutput()
+					if err != nil {
+						if maxRetry < retryCounter {
+							return err
+						}
+						log.Errorf("git command exec: %+v, msg: %+v", err, string(out))
+						retryCounter++
+						continue
+					}
+					retryCounter = 0 // reset
+				}
+			}
+		})
+
+		// sync hook API
+		eg.Go(func() error {
+			s := newServer(string(port))
+			return s.run(ctx)
+		})
+
+		// self sync cycle
+		eg.Go(func() error {
+			ticker := time.NewTicker(1 * time.Hour)
+			for {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+
+				case <-ticker.C:
+					err = syncDiff()
+					if err != nil {
+						log.Errorf("self sync cycle: %+v", err)
+						continue
+					}
+				}
+			}
+		})
+
+		quit := make(chan os.Signal, 1)
+		defer close(quit)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		log.Info("start sync-agent")
+
+		select {
+		case <-quit:
+			cancel()
+		case <-ctx.Done():
+		}
+
+		if err := eg.Wait(); err != nil {
+			log.Error(err)
+		}
+
+		log.Info("done")
+	},
+}
+
+func postFiles(workdir string) ([]string, error) {
 	var result []string
 	err := filepath.Walk(workdir, func(path string, info os.FileInfo, err error) error {
 		if !isPostMdfile(path) {
@@ -88,7 +178,7 @@ func allFilePath(workdir string) ([]string, error) {
 	return result, nil
 }
 
-func gitDiffFiles() ([]string, error) {
+func gitDiffPostFiles() ([]string, error) {
 	cmd := exec.Command("git", "rev-parse", "HEAD")
 	cmd.Dir = root
 	revision, err := cmd.CombinedOutput()
@@ -123,21 +213,6 @@ func gitDiffFiles() ([]string, error) {
 	return posts, nil
 }
 
-func sendPost(r request) error {
-	reqJSON, err := json.Marshal(r)
-	if err != nil {
-		return err
-	}
-	res, err := http.Post(url, "application/json", bytes.NewBuffer(reqJSON))
-	if err != nil {
-		return err
-	}
-	if res.StatusCode != 200 {
-		return errors.New("status is not OK")
-	}
-	return nil
-}
-
 func syncPost(files []string) error {
 	for _, filepath := range files {
 		source, err := ioutil.ReadFile(filepath)
@@ -166,104 +241,87 @@ func syncPost(files []string) error {
 			UpdatedAt:   time.Now(),
 		}
 
-		err = sendPost(req)
+		reqJSON, err := json.Marshal(req)
 		if err != nil {
 			return err
 		}
+		res, err := http.Post(url, "application/json", bytes.NewBuffer(reqJSON))
+		if err != nil {
+			return err
+		}
+		if res.StatusCode != 200 {
+			return errors.New("status is not OK")
+		}
+		log.Infof("sync: %+v", m.id)
 	}
 	return nil
 }
 
-// newCmd represents the new command
-var syncCmd = &cobra.Command{
-	Use:   "sync",
-	Short: "sync post external store",
-	Long:  "sync post external store",
-	Run: func(cmd *cobra.Command, args []string) {
-		var filepaths []string
-		var err error
-		if !agentMode {
-			filepaths, err = allFilePath(workdir)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			err = syncPost(filepaths)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			return
-		}
+func syncDiff() error {
+	filepaths, err := gitDiffPostFiles()
+	if err != nil {
+		return err
+	}
+	err = syncPost(filepaths)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-		eg, ctx := errgroup.WithContext(ctx)
+type server struct {
+	server http.Server
+}
 
-		// git helthz
-		eg.Go(func() error {
-			var retryCounter int
-			ticker := time.NewTicker(30 * time.Second)
-			for {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case <-ticker.C:
-					cmd := exec.Command("git", "status")
-					cmd.Dir = root
-					out, err := cmd.CombinedOutput()
-					if err != nil {
-						if maxRetry < retryCounter {
-							return err
-						}
-						log.Errorf("git command exec: %+v, msg: %+v", err, string(out))
-						retryCounter++
-						continue
-					}
-					retryCounter = 0 // reset
-				}
-			}
+func router() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
+
+	rg := r.Group("api/v1")
+	{
+		rg.GET("/healthz", func(c *gin.Context) {
+			c.JSON(http.StatusOK, "ok!")
 		})
-
-		// agent cycle
-		eg.Go(func() error {
-			ticker := time.NewTicker(30 * time.Second)
-			for {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-
-				case <-ticker.C:
-					filepaths, err = gitDiffFiles()
-					if err != nil {
-						log.Errorf("get diff files: %+v", err)
-						continue
-					}
-					err = syncPost(filepaths)
-					if err != nil {
-						log.Errorf("sync post: %+v", err)
-						continue
-					}
-				}
+		rg.GET("/sync", func(c *gin.Context) {
+			err := syncDiff()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
 			}
+			c.JSON(http.StatusOK, gin.H{"msg": "success data sync"})
 		})
+	}
+	return r
+}
 
-		quit := make(chan os.Signal, 1)
-		defer close(quit)
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+func newServer(port string) *server {
+	return &server{
+		server: http.Server{
+			Addr:    port,
+			Handler: router(),
+		},
+	}
+}
 
-		log.Info("start sync-agent")
+func (s *server) run(ctx context.Context) error {
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return s.server.ListenAndServe()
+	})
 
-		select {
-		case <-quit:
-			cancel()
-		case <-ctx.Done():
-		}
+	<-ctx.Done()
+	sCtx, sCancel := context.WithTimeout(
+		context.Background(), 10*time.Second,
+	)
+	defer sCancel()
+	if err := s.server.Shutdown(sCtx); err != nil {
+		return err
+	}
 
-		if err := eg.Wait(); err != nil {
-			log.Error(err)
-		}
-
-		log.Info("done")
-	},
+	return eg.Wait()
 }
